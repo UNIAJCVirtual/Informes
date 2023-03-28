6f71051433e310dcc9a31b44d00ddaf126d96b00
UNIAJCVirtual <aulasvirtuales40@gmail.com>
2023-03-28 14:41:03 -0500

    Update

--- services/connection.php
+++ services/connection.php
@@ -1,14 +1,15 @@
 <?php
 	function connection()
 	{
+		/*
 		$server = "uniajctmp.chf279hfbxqe.us-east-1.rds.amazonaws.com";
 		$user = "aulasvirtuales";
 		$pass = "ioxlFNdODPLZkMXU";
 		$database = "uniajc";
 		$connection = new mysqli($server, $user, $pass, $database);
 		return $connection;
+		*/
 		
-		/*
 		$server = "localhost";
 		$user = "root";
 		$pass = "";
@@ -16,7 +17,6 @@
 		$connection = new mysqli($server, $user, $pass, $database);
 		$connection->set_charset("utf8");
 		return $connection;
-		*/
 		
 		//en base de datos hay crear un usuariopara localhost y otro para %
 	}
